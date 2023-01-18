24fc14d3f184832498ae56f65a6196329870c15c
RohitSawant0351 <rohits1547@gmail.com>
2023-01-19 00:01:21 +0530

    2 new carousel added at homepage

---
+++ myapp/src/Components/Carousel/MensMultiProdCarousel.jsx
@@ -0,0 +1,54 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import axios from "axios";
+import Carousel from "nuka-carousel"
+import React, { useEffect, useState } from "react";
+
+const MensMultiProdCarousel = () => {
+  const [mensData, setmensData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://mybasket.onrender.com/category/mens?page=1&limit=20")
+      .then((res) => {
+        setmensData(res.data);
+        console.log(res.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Carousel renderBottomCenterControls={"disabled"} slidesToShow={7}>
+        {mensData &&
+          mensData.map((item) => {
+            return (
+              <Box className="multi_product_carousel_prod" key={item._id}>
+                <img src={item.item_img} alt="" />
+                <Box display={"flex"} flexDirection={"column"} gap={"10px"}>
+                  <Text>{item.item_name} </Text>
+                </Box>
+                <Box>                 
+                  <Text marginBottom={"2%"} fontSize={"xs"} fontWeight={"bold"} color={"green"}>
+                    {item.discount}
+                  </Text>
+                </Box>
+                <hr />
+                <Box
+                  display={"flex"}
+                  justifyContent={"space-around"}
+                  alignItems={"baseline"}
+                >
+                  <Text fontSize={"lg"}>₹ {item.item_disc_price}</Text>
+                  <Text textDecoration={"line-through"}>
+                    ₹ {item.item_mrp}
+                  </Text>
+                  <Button>View</Button>
+                </Box>
+              </Box>
+            );
+          })}
+      </Carousel>
+    </div>
+  );
+};
+
+export default MensMultiProdCarousel;
--- myapp/src/Components/Home/HomeII.jsx
+++ myapp/src/Components/Home/HomeII.jsx
@@ -1,6 +1,5 @@
 import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import Carousel from 'nuka-carousel';
 import { useState } from "react";
 import img1 from "../.././Assets/banner1.jpg";
 import img2 from "../.././Assets/banner2.jpg";
@@ -8,13 +7,13 @@ import img3 from "../.././Assets/banner3.jpg";
 import img4 from "../.././Assets/banner4.jpg";
 import img5 from "../.././Assets/banner5.png";
 import img6 from "../.././Assets/banner6.png";
-import { useEffect } from "react";
-import axios from "axios";
+import MensMultiProdCarousel from "../Carousel/MensMultiProdCarousel";
+import WomensMultiProdCarousel from "../Carousel/WomenMultiProdCarousel";
+
 
 const HomeII = () => {
   let [banner, setBanner] = useState(0);
   const [currentBanner, setCurrentBanner] = useState(img1);
-  const [mensData, setmensData] = useState([])
 
   const bannerAds = [img1, img2, img3, img4, img5, img6];
 
@@ -30,66 +29,39 @@ const HomeII = () => {
 
   const handleRem = () => {
     if (banner === bannerAds.length - 1) {
-      setBanner(0)
+      setBanner(0);
     } else {
-        setBanner((prev) => prev + 1);
+      setBanner((prev) => prev + 1);
     }
-    setCurrentBanner(bannerAds[banner])
+    setCurrentBanner(bannerAds[banner]);
   };
 
-
-  const handleSrc= (e)=> {
+  const handleSrc = (e) => {
     console.log(e.target.src);
-  }
+  };
 
-  useEffect(()=>{
-    axios.get("https://mybasket.onrender.com/category/mens?page=1&limit=25")
-    .then((res)=>{
-      setmensData(res.data)
-      console.log(res.data)
-    })
-  },[])
-  
 
 
   return (
     <div>
-       {/* Carousel =====> */}
-
-     <Box className="carousel-container">
+      {/* Home Main Carousel =====> */}
+      <Box className="carousel-container">
         <Button onClick={handleAdd}> ❮ </Button>
-        
         <Button onClick={handleRem}> ❯ </Button>
         <img onClick={handleSrc} src={currentBanner} alt="" />
       </Box>
-
       {/* fashion ===> caraousel */}
-      <br />
-      <Box>
-        <Carousel  slidesToShow={10}>
-          {mensData && mensData.map((item)=>{
-              return (
-                <Box>
-                    <img src={item.item_img} alt="" />
-                    <Heading size={"sm"}></Heading>
-                    <Box display={"flex"} gap={"10px"}>
-                      <b>Discount price: </b>
-                      <Text></Text>
-                    </Box>
-                    <Box display={"flex"} gap={"10px"}>
-                      <b>M.R.P :</b>
-                      <Text></Text>
-                    </Box>
-                    <Box display={"flex"} gap={"10px"}>
-                      <b>Discount :</b>
-                      <Text></Text>
-                    </Box>
-                </Box>
-              )
-          })}
-        </Carousel>
-      </Box>
 
+      <br />
+      <Box className="multi_product_carousel">
+        <Heading fontSize={"xl"}> Trending in Men's</Heading>
+        <br />
+        <MensMultiProdCarousel/>
+        <br />
+        <Heading fontSize={"xl"}> Trending in Women's</Heading>
+        <br />
+        <WomensMultiProdCarousel/>
+      </Box>
     </div>
   );
 };
